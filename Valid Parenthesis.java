5a4a0f95ea1a833b247a20bcf9c8bc0646db36ef
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2026-02-16 10:08:32 +0530

    Create Valid Parenthesis.java

---
+++ Valid Parenthesis.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class Main
+{
+    public static boolean isValid(String s)
+    {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (stack.isEmpty())
+                {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '['))
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        System.out.println(isValid(s));
+    }
+}
