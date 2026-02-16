3f5bbc4cab6fdc8c8dc32e74839352dc91e78e1d
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2026-02-16 10:28:53 +0530

    Implement character counting in Counting Characters.java
    Added functionality to count and display character occurrences in a string.

--- Counting Characters.java
+++ Counting Characters.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+import java.util.HashMap;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : map.keySet())
+        {
+            System.out.println(c + " -> " + map.get(c));
+        }
+    }
+}
 
