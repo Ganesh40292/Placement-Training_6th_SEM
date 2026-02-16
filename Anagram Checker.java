839829a0858404840d583bea9b73549f3ba4f7bc
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2026-02-16 10:25:58 +0530

    Create Anagram Checker.java

---
+++ Anagram Checker.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main
+{
+    public static boolean isAnagram(String s, String t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(isAnagram(s, t));
+    }
+}
