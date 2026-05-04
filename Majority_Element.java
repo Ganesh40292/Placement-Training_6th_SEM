473d3743c558df05e4817c891b5c5008331c880f
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2026-05-04 09:33:48 +0530

    Implement Majority Element finder in Java

---
+++ Majority_Element.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+
+            if (num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        System.out.println(candidate);
+    }
+}
